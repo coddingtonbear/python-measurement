9dae7a7169d3a15125a595976c880348f463dd0f
Adam Coddington <me@adamcoddington.net>
2013-07-20 16:49:12 -0700

    Bumping version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as in_:
 
 setup(
     name='measurement',
-    version='1.1',
+    version='1.2',
     url='http://bitbucket.org/latestrevision/python-measurement/',
     description='Easily use and manipulate unit-aware measurements in Python',
     author='Adam Coddington',
