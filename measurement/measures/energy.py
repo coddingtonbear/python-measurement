8c8f26ffe52e2df68a8f7ba87871f260a5023406
Konstantin Schukraft <konstantin@schukraft.org>
2015-08-26 02:13:39 +0200

    Correct factor for electron volt

--- measurement/measures/energy.py
+++ measurement/measures/energy.py
@@ -12,7 +12,7 @@ class Energy(MeasureBase):
         'c': 4.18400,
         'C': 4184.0,
         'J': 1.0,
-        'eV': 1.602177e-9,
+        'eV': 1.602177e-19,
         'tonne_tnt': 4184000000,
     }
     ALIAS = {
