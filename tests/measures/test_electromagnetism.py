e71417d900462b2d4ac08e93372b044b444b60ef
Tobias Pfeiffer <tgp@preferred.jp>
2020-04-27 16:56:49 +0900

    append "-> None" to all test functions

--- tests/measures/test_electromagnetism.py
+++ tests/measures/test_electromagnetism.py
@@ -2,35 +2,35 @@ from measurement import measures
 
 
 class TestCurrent:
-    def test_mul(self):
+    def test_mul(self) -> None:
         assert measures.Current("2 A") * measures.Voltage(
             "12 V"
         ) == measures.ElectricPower("24 W")
 
-    def test_mul__super(self):
+    def test_mul__super(self) -> None:
         assert measures.Current("2 A") * 2 == measures.Current("4 A")
 
 
 class TestVoltage:
-    def test_mul(self):
+    def test_mul(self) -> None:
         assert measures.Voltage("12 V") * measures.Current(
             "2 A"
         ) == measures.ElectricPower("24 W")
 
-    def test_mul__super(self):
+    def test_mul__super(self) -> None:
         assert measures.Voltage("6 V") * 2 == measures.Voltage("12 V")
 
 
 class TestElectricPower:
-    def test_truediv__voltage(self):
+    def test_truediv__voltage(self) -> None:
         assert measures.ElectricPower("24 W") / measures.Voltage(
             "12 V"
         ) == measures.Current("2 A")
 
-    def test_truediv__current(self):
+    def test_truediv__current(self) -> None:
         assert measures.ElectricPower("24 W") / measures.Current(
             "4 A"
         ) == measures.Voltage("6 V")
 
-    def test_truediv__super(self):
+    def test_truediv__super(self) -> None:
         assert measures.ElectricPower("24 W") / 2 == measures.ElectricPower("12 W")
--- tests/measures/test_speed.py
+++ tests/measures/test_speed.py
@@ -2,10 +2,10 @@ from measurement.measures.mechanics import Speed
 
 
 class TestSpeed:
-    def test_attrconversion(self):
+    def test_attrconversion(self) -> None:
         assert Speed("10 m/s") == Speed("36 km/h")
 
-    def test_addition(self):
+    def test_addition(self) -> None:
         train_1 = Speed(mile__hour=10)
         train_2 = Speed(mile__hour=5)
 
@@ -14,7 +14,7 @@ class TestSpeed:
 
         assert actual_value == expected_value
 
-    def test_iadd(self):
+    def test_iadd(self) -> None:
         train_1 = Speed(mile__hour=10)
         train_2 = Speed(mile__hour=5)
 
@@ -24,7 +24,7 @@ class TestSpeed:
 
         assert actual_value == expected_value
 
-    def test_sub(self):
+    def test_sub(self) -> None:
         train_1 = Speed(mile__hour=10)
         train_2 = Speed(mile__hour=5)
 
@@ -33,7 +33,7 @@ class TestSpeed:
 
         assert expected_value == actual_value
 
-    def test_isub(self):
+    def test_isub(self) -> None:
         train_1 = Speed(mile__hour=10)
         train_2 = Speed(mile__hour=5)
 
@@ -43,7 +43,7 @@ class TestSpeed:
 
         assert expected_value == actual_value
 
-    def test_mul(self):
+    def test_mul(self) -> None:
         train_1 = Speed(mile__hour=10)
         multiplier = 2
 
@@ -52,7 +52,7 @@ class TestSpeed:
 
         assert expected_value == actual_value
 
-    def test_imul(self):
+    def test_imul(self) -> None:
         train_1 = Speed(mile__hour=10)
         multiplier = 2
 
@@ -62,7 +62,7 @@ class TestSpeed:
 
         assert expected_value == actual_value
 
-    def test_div(self):
+    def test_div(self) -> None:
         train_1 = Speed(mile__hour=10)
         divider = 2
 
@@ -71,7 +71,7 @@ class TestSpeed:
 
         assert expected_value == actual_value
 
-    def test_idiv(self):
+    def test_idiv(self) -> None:
         train_1 = Speed(mile__hour=10)
         divider = 2
 
@@ -81,42 +81,42 @@ class TestSpeed:
 
         assert expected_value == actual_value
 
-    def test_equals(self):
+    def test_equals(self) -> None:
         train_1 = Speed(mile__hour=10)
         train_2 = Speed(mile__hour=10)
 
         assert train_1 == train_2
 
-    def test_lt(self):
+    def test_lt(self) -> None:
         train_1 = Speed(mile__hour=5)
         train_2 = Speed(mile__hour=10)
 
         assert train_1 < train_2
 
-    def test_bool_true(self):
+    def test_bool_true(self) -> None:
         train_1 = Speed(mile__hour=5)
 
         assert train_1
 
-    def test_bool_false(self):
+    def test_bool_false(self) -> None:
         train_1 = Speed(mile__hour=0)
 
         assert not train_1
 
-    def test_abbreviations(self):
+    def test_abbreviations(self) -> None:
         train_1 = Speed(mph=4)
         train_2 = Speed(mile__hour=4)
 
         assert train_1 == train_2
 
-    def test_different_units_addition(self):
+    def test_different_units_addition(self) -> None:
         train = Speed(km__h=1)
         increase = Speed(m__h=10)
 
         assert train + increase == Speed("1.01 km/h")
 
-    def test_mph(self):
+    def test_mph(self) -> None:
         assert Speed(mph=10) == Speed(mi__h=10), Speed._units["mi__h"].factor
 
-    def test_kph(self):
+    def test_kph(self) -> None:
         assert Speed(kph=10) == Speed(km__h=10), Speed._units["km__h"].factor
