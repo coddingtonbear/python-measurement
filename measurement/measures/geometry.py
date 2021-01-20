cb67a1bdf9541b864f4259202abcb31db6695bee
Omen Apps <support@omenapps.com>
2021-01-19 23:01:06 -0500

    Improved docstrings and other strings
    - Corrected spelling/grammar
    - Added additional context

--- measurement/measures/geometry.py
+++ measurement/measures/geometry.py
@@ -7,24 +7,24 @@ __all__ = ["Distance", "Area", "Volume"]
 
 class Distance(AbstractMeasure):
     """
-    A distance the factor for both :class:`Area` and :class:`Volume`.
+    Distance is a factor for both :class:`Area` and :class:`Volume`.
 
     If you multiply a :class:`Distance` with another :class:`Distance`,
-    you will get an :class:`Area`:
+    you will get a :class:`Area`:
 
         >>> from measurement import measures
         >>> measures.Distance('2 m') * measures.Distance('3 m')
         Area(metre²="6")
 
-    If you multiply a :class:`Distance` with an :class:`Area` or two
-    :class:`Distances<Distance>`, you will get an :class:`Volume`:
+    If you multiply a :class:`Distance` with a :class:`Area` or two
+    :class:`Distances<Distance>`, you will get a :class:`Volume`:
 
         >>> from measurement import measures
         >>> measures.Distance('2 m') * measures.Area('6 m²')
         Volume(metre³="12")
 
-    You can also build the second and thrid power of a :class:`Distance`,
-    to get an :class:`Area` or :class:`Volume`.
+    You can also build the second and third power of a :class:`Distance`,
+    to get a :class:`Area` or :class:`Volume`.
 
         >>> from measurement import measures
         >>> measures.Distance('2 m') ** 2
@@ -111,8 +111,8 @@ class Area(AbstractMeasure, metaclass=AreaBase):
     """
     An area is defined as :class:`Distance` multipled by :class:`Distance`.
 
-    This is why you can multiple two :class:`Distances<Distance>` to get
-    an :class:`Area` or devide an :class:`Area` to get a :class:`Distance`:
+    This is why you can multiply two :class:`Distances<Distance>` to get
+    an :class:`Area` or divide an :class:`Area` to get a :class:`Distance`:
 
         >>> from measurement import measures
         >>> measures.Distance('2 m') * measures.Distance('3 m')
@@ -178,9 +178,9 @@ class Volume(AbstractMeasure, metaclass=VolumeBase):
     """
     A volume is defined as :class:`Area` multipled by :class:`Distance`.
 
-    This is why you can multiple three :class:`Distances<Distance>` to get
-    a :class:`Volume`, multiple an :class:`Area` by a :class:`Distance`
-    or devide a :class:`Volume` by both :class:`Distance` and :class:`Area`:
+    This is why you can multiply three :class:`Distances<Distance>` to get
+    a :class:`Volume`, multiply a :class:`Area` by a :class:`Distance`
+    or divide a :class:`Volume` by both :class:`Distance` and :class:`Area`:
 
         >>> from measurement import measures
         >>> measures.Distance('2 m') * measures.Distance('3 m') * measures.Distance('4 m')
